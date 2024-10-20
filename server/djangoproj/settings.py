dbf33ee6c4916e45edf054b10e31655797a4a49d
Ismael Coulibaly <coulbaly.ismael@gmail.com>
2024-10-19 23:45:51 -0400

    fixed sign up

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -145,5 +145,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
-ALLOWED_HOSTS=['localhost', 'https://aboucoulibal-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
-CSRF_TRUSTED_ORIGINS=['https://aboucoulibal-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
+ALLOWED_HOSTS=['localhost', 'https://aboucoulibal-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS=['https://aboucoulibal-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
